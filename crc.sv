87de484d7e0838058e6061ee7d90bb14d90a92de
cwill <c.williamso@gmail.com>
2014-10-27 01:03:22 -0400

    Fix sending-CRC

--- crc.sv
+++ crc.sv
@@ -5,17 +5,20 @@
  **/
 
 
-module crc
+module crc_encode
 (input logic clk, rst_L,
  input logic pause_out,
- input logic inb, recving,
+ input logic inb, recving, pkttype, // pkttype = 1 for 16-bit crc, 0 for 5-bit
  input logic start,
  output logic pause_in,
  output logic outb, sending);
 
   enum logic [1:0] {IDLE, CALCCRC, SENDCRC} state, nextState;
+
+  logic crc_type;
   
-  logic [4:0] crc_save;
+  logic [4:0] crc5_result;
+  logic [15:0] crc16_result;
   logic crc5_x2_inb, crc5_x2_outb, crc5_x5_inb, crc5_x5_outb;
 
   logic crc16_x2_inb, crc16_x2_outb;
@@ -23,32 +26,32 @@ module crc
   logic crc16_x16_inb, crc16_x16_outb;
 
   logic shift_crc, init_crc;
-  logic [3:0] curcount;
+  logic [4:0] curcount;
   logic clear_count, inc_count;
 
   /* 5-bit CRC */
-  crc_shiftreg #(2) crc5_x2(.Q(crc_save[1:0]), .inb(crc5_x2_inb), 
+  crc_shiftreg #(2) crc5_x2(.Q(crc5_result[1:0]), .inb(crc5_x2_inb), 
                     .outb(crc5_x2_outb),.shift(shift_crc), .clr(init_crc), 
                     .rst_b(rst_L), .*);
 
-  crc_shiftreg #(3) crc5_x5(.Q(crc_save[4:2]), .inb(crc5_x5_inb), 
+  crc_shiftreg #(3) crc5_x5(.Q(crc5_result[4:2]), .inb(crc5_x5_inb), 
                     .outb(crc5_x5_outb),.shift(shift_crc), .clr(init_crc), 
                     .rst_b(rst_L), .*);
   
   /* 16-bit CRC */
-  crc_shiftreg #(2) crc16_x2(.Q(), .inb(crc16_x2_inb), 
+  crc_shiftreg #(2) crc16_x2(.Q(crc16_result[1:0]), .inb(crc16_x2_inb), 
                     .outb(crc16_x2_outb),.shift(shift_crc), .clr(init_crc), 
                     .rst_b(rst_L), .*);
 
-  crc_shiftreg #(13) crc16_x15(.Q(), .inb(crc16_x15_inb), 
+  crc_shiftreg #(13) crc16_x15(.Q(crc16_result[14:2]), .inb(crc16_x15_inb), 
                     .outb(crc15_x2_outb),.shift(shift_crc), .clr(init_crc), 
                     .rst_b(rst_L), .*);
 
-  crc_shiftreg #(1) crc16_x16(.Q(), .inb(crc16_x16_inb), 
+  crc_shiftreg #(1) crc16_x16(.Q(crc16_result[15]), .inb(crc16_x16_inb), 
                     .outb(crc16_x16_outb),.shift(shift_crc), .clr(init_crc), 
                     .rst_b(rst_L), .*);
   
-  counter #(4) outcrc_remaining(.inc_cnt(inc_count), .up(1'b1), .cnt(curcount), 
+  counter #(5) outcrc_remaining(.inc_cnt(inc_count), .up(1'b1), .cnt(curcount), 
                                 .clr_cnt(clear_count), .rst_b(rst_L), .*);
 
   assign crc5_x2_inb = crc5_x5_outb ^ inb;
@@ -61,8 +64,10 @@ module crc
   always_ff @(posedge clk, negedge rst_L)
   if (~rst_L)
     state <= IDLE;
-  else
+  else begin
+    if (recving) crctype <= pkttype; // Remember pkt type after done receiving
     state <= nextState;
+  end
 
   always_comb begin
     shift_crc = 0;
@@ -96,25 +101,43 @@ module crc
         if (~recving) begin
           shift_crc = 0;
           clear_count = 0;
-          inc_count = 1;
-          outb = ~crc_save[4 - curcount];
-          nextState = SENDCRC;
-        end
-        else 
+          inc_count = 0;
+          if (crctype == 1'b0) begin
+            outb = ~crc5_result[4 - curcount];
+            nextState = SENDCRC5;
+          end
+          else begin
+            outb = ~crc5_result[15 - curcount];
+            nextState = SENDCRC16;
+          end
+        else
           nextState = CALCCRC;
       end
-      SENDCRC: begin
+      SENDCRC5: begin
         sending = 1;
         pause_in = 1;
         inc_count = 1;
-        outb = ~crc_save[4 - curcount];
+        outb = ~crc5_result[4 - curcount];
         if (curcount == 4)begin
           nextState = IDLE;
           init_crc = 1;
         end
         else 
-          nextState = SENDCRC;
+          nextState = SENDCRC5;
       end
+      SENDCRC16: begin
+        sending = 1;
+        pause_in = 1;
+        inc_count = 1;
+        outb = ~crc5_result[15 - curcount];
+        if (curcount == 15)begin
+          nextState = IDLE;
+          init_crc = 1;
+        end
+        else 
+          nextState = SENDCRC16;
+      end
+
     endcase
 
   end
