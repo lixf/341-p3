7910d11e6fced221cb95574b113014be3d9425b8
Xiaofan Li <lixf@cmu.edu>
2014-10-27 21:23:38 -0400

    bug fix and top module

--- usbHost.sv
+++ usbHost.sv
@@ -9,32 +9,7 @@
 
 module usbHost
   (input logic clk, rst_L, 
-  usbWires wires);
- 
-  //testbench style to talk to the bitstream encoder
-  logic pause_bit_stuff,pktready_bs;
-  logic outb_bs,sending_bs,gotpkt_bs;
-  logic pause_bs, pause_crc, outb_crc, outb_bit, outb_nrzi;
-  logic sending_nrzi, pktend, usb_ready, sop;
-  logic [3:0] pid, endp;
-  logic [6:0] addr;
-  logic [63:0] data;
-  
-  bitstream_encoder be(.pause(pause_bs),.pktready(pktready_bs),
-        .outb(outb_bs),.sending(sending_bs),.gotpkt(gotpkt_bs),
-        .start(sop),.*);
-  crc c(.pause_out(pause_bit_stuff),.inb(outb_bs),.recving(sending_bs),
-        .pause_in(pause_crc),.outb(outb_crc),.sending(sending_crc),
-        .start(sop),.*);
-  bit_stuff bs(.inb(outb_crc),.outb(outb_bit),.pause(pause_bit_stuff),
-        .start(sop),.*);
-  nrzi n(.inb(outb_bit),.outb(outb_nrzi),.*);
-  to_usb tu(.data_bit(outb_nrzi),.data_start(sending_bs),.data_end(pktend),
-        .d_p(wires.DP),.d_m(wires.DM),.ready(usb_ready),.*);
-  
-  assign pktend = (~sending_bs) & (~sending_crc),
-         pause_bs = pause_bit_stuff | pause_crc;
-  /* Tasks needed to be finished to run testbenches */
+  usbWires wires); 
   
   task prelabRequest
   // sends an OUT packet with ADDR=5 and ENDP=4
@@ -47,43 +22,10 @@ module usbHost
     addr <= 7'd5;
     endp <= 4'd4;
     pktready_bs <= 1;
-    @(posedge clk);
+    ##1;
     pktready_bs <= 0;
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-  
+    ##30;
+     
   endtask: prelabRequest
 
   task readData
@@ -103,8 +45,56 @@ module usbHost
    output bit        success);
 
   endtask: writeData
+  
+  //use clocking
+  default clocking myDelay
+    @(posedge clk);
+  endclocking 
+  
+  //signal for RW FSM
+  logic read, tran_ready; // Set by top task!
+  logic recv_ready_up; // the recv_ready signal to pass up
+  logic [63:0] data_down_rw;  // DATA from top task!
+  logic [63:0] data_up_rw;    // DATA report to task!
+  //mempage is implicit
+  logic trans_finish, unsuccess; // passed up
+  logic free;             // to R/W FSM
+  logic bad;              // from protocol FSM
+  //recv_ready passed up 
+  logic [63:0] data_down_pro;
+  logic [63:0] data_up_pro; 
+  logic send_in;          // from R/W FSM to send a IN 
+  logic input_ready;      // control signal from R/W FSM
+  logic [6:0] addr; 
+  logic [3:0] endp;    
+   
+  // protocol FSM signals
+  logic [63:0] data_out_pro;  //out of the protocol
+  logic [63:0] data_in_pro;   //into the protocol
+  logic cancel;          // cancel this transaction
+  logic recv_ready;      // data received and ready to be read
+  
+  logic down_input;       // control signal from down stream
+  logic down_ready;       // if the downstream is ready to receive
+  logic corrupted;        // asserted if the data is corrupted
+  logic ack;              // received a ack
+  logic nack;             // received a nack
+  logic pktready;        // to downstream senders
+  logic [3:0] pid_out; 
+  logic [6:0] addr_out; 
+  logic [3:0] endp_out;
 
-  // usbHost starts here!!
+  // pipeline-out signals
+
+
+  // The top module for all usb moules
+  ReadWrite rw(.recv_ready_pro(recv_ready),.recv_ready(recv_ready_up),
+               .done(tran_finish),.cancel(unsuccess),.*);
+  ProtocolFSM pro(.data(data_down_pro),.data_in(data_in_pro),
+                  .data_recv(data_up_pro),.data_out(data_out_pro).*);
+  pipeIn pi();
+  pipeOut po(.pid(pid_out),.endp(endp_out),.addr(addr_out),
+             .pktready_bs(pktready),.data(data_out_pro),.*);
 
 
 endmodule: usbHost
